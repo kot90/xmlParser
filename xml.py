4952a18f50deefa9482b94d0ee3db3cbfa08664f
John HB <jhbco@hotmail.com>
2014-03-24 22:10:31 -0500

    sec commit

--- xml.py
+++ xml.py
@@ -4,4 +4,18 @@ path = '/Users/jhbco/Desktop/books.xml'
 parsed = objectify.parse(open(path))
 root = parsed.getroot()
 
-print parsed
+data = []
+
+
+for i in root.book:
+	book_data = {}
+	for child in i.getchildren():
+		book_data[child.tag] = child.pyval
+	data.append(book_data)
+
+#print data[0]['description']
+print len(data)
+
+for j in data:
+	temp = j['description']
+	print temp.strip()
